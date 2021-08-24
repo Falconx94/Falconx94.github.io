e816d8f8693b908f82b12431a26eda8dc5855e26
Ricardo Mascareño Reza <richin10@hotmail.com>
2021-08-23 22:42:59 -0600

    Cambiado Form de PHP a JS para Whats

---
+++ assets/js/whatsapp.js
@@ -0,0 +1,36 @@
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '526871253439';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let email = document.querySelector('#email').value
+        let asunto = document.querySelector('#asunto').value
+        let message = document.querySelector('#message').value
+        let mensaje = 'send?phone=' + telefono + '&text=Hola soy%0A' + nombre + ' te escribo por el asunto de:' + asunto + 'Mi correo electrónico es: %0A' + email + '%0A' + message +''
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
