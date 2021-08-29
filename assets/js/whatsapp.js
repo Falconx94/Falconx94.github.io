0840ca94607c53574d4ca00a89a1a5cadb59cdaf
Ricardo Mascareño Reza <richin10@hotmail.com>
2021-08-29 16:31:19 -0600

    Telefono corregido a Whats

--- assets/js/whatsapp.js
+++ assets/js/whatsapp.js
@@ -13,7 +13,7 @@ const formulario = document.querySelector('#formulario');
 const buttonSubmit = document.querySelector('#submit');
 const urlDesktop = 'https://web.whatsapp.com/'; //'https://web.whatsapp.com/'https://api.whatsapp.com/send/?text=
 const urlMobile = 'whatsapp://';
-const telefono = '526871253439';
+const telefono = '526871732226';
 
 formulario.addEventListener('submit', (event) => {
     event.preventDefault()
